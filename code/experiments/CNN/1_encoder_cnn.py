69dd677f015ddc4902d54ab6823395450d24f191
CSULB Data Science Lab <66384937+csulb-datascience@users.noreply.github.com>
2021-04-23 00:21:52 -0700

    Add files via upload

---
+++ code/experiments/CNN/1_encoder_cnn.py
@@ -0,0 +1,108 @@
+# csulb-datascience
+#
+# Program to train the models.
+#   the results are saved in a csv file "summary.csv"
+#
+# Authors: 
+#      Nelson Minaya, email: nelson.minaya@student.csulb.edu
+#      Nhat Anh Le,   email: nhat.le01@student.csulb.edu
+#
+# Date: June 2020
+#
+# Include a reference to this site if you will use this code.
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # use id from $ nvidia-smi
+
+
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import time
+
+#classes
+import sys
+sys.path.append("../../classes")
+from Recognition_Dataset_V1 import Dataset
+from Recognition_EncoderModel_V2 import EncoderModel
+from Recognition_EmbeddingsDataset_V1 import EmbeddingsDataset
+
+
+#Save the header for the CSV file
+def saveHeader(saveAs):
+    #save the header
+    values = [["iteration","lossTrain","lossValid", "samplesTrain", "samplesValid", "time"]]
+    data = pd.DataFrame(data= values)    
+    data.to_csv(saveAs, header=None, mode="a") 
+    
+#Save the results of testing 
+def save(saveAs, iteration, lossTrain, lossValid, samplesTrain, samplesValid, delta):
+    #get values:               
+    values = [[iteration, lossTrain, lossValid, samplesTrain, samplesValid, delta]]
+    data = pd.DataFrame(data= values)    
+    data.to_csv(saveAs, header=None, mode="a")     
+    
+#CSV file
+saveAs = "./summary_encoder_cnn.csv"
+saveHeader(saveAs)    
+    
+#load the dataset
+pathData = "../../data"
+fileData = "data_cond1_c2"
+
+print("--> loading the datasets")
+dataset = Dataset(pathData, fileData+".csv")
+print("--> datasets ready")
+
+#parameters
+numberPeopleTraining = 16
+numberPeopleKnown = 7
+learningRate = 0.001
+alpha = 1.0
+beta = 1.0
+epochs =  20
+batchSize = 64
+iterations=20
+
+print("Training encoders")    
+for i in range(1, iterations+1):
+    print("\n--> Iteration: ", i)
+    
+    #Split the dataset for: training, validation, unknown, test
+    dataset.split(numberPeopleTraining, numberPeopleKnown)
+    dataset.saveSets(".","iter_" + str(i) + "_dataset.npy")
+    x_train, y_train, m_train = dataset.getDataset(dataset.trainingSet, batchSize=batchSize)
+    x_valid, y_valid, m_valid = dataset.getDataset(dataset.validationSet, batchSize=batchSize)
+        
+    # Creates a session 
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.Session(config=config)
+            
+    with session.as_default():                    
+        t0 = time.time()
+        print("--> training...")
+        encoderModel = EncoderModel()
+        encoder = encoderModel.getCompiledCNN(dataset.unitSize(), alpha, beta, learningRate)
+        history = encoder.fit(x_train, y_train, batch_size = batchSize, epochs = epochs, validation_data = (x_valid, y_valid))        
+        lossTrain, lossValid = encoderModel.getResults(history)
+
+        #Save results for encoder
+        print("\n--> saving ")
+        #encoder.save("iter_" + str(i) + "_model.h5")
+        embeddings = EmbeddingsDataset(encoder, dataset)
+        embeddings.predictEmbeddings()
+        embeddings.save(".", "iter_" + str(i) + "_embeddings.npy")
+                
+        t1 = time.time()
+        save(saveAs, i, lossTrain, lossValid, len(y_train), len(y_valid), t1-t0)
+
+    tf.compat.v1.reset_default_graph()
+    session.close()            
+
+
+
+
+
---
+++ code/experiments/LSTM/2_svm_lstm.py
@@ -0,0 +1,97 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # use id from $ nvidia-smi
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import time
+
+
+#Developed classes
+import sys
+sys.path.append("../../classes")
+from Recognition_Dataset_V1 import Dataset
+from Recognition_EncoderModel_V2 import EncoderModel
+from Recognition_EmbeddingsDataset_V1 import EmbeddingsDataset
+from Recognition_SVM_V4 import SVM
+
+
+def saveHeader(saveAs):
+    #save the header
+    values = [["iteration", "gamma", "nu", "tau", "noise", "TPR", "TNR", "accuracy", "TP", "FP", "FN", "TN", "total in train", "total not in train", "fp-3-3", "fn-2-3", "time"]]
+    data = pd.DataFrame(data= values)    
+    data.to_csv(saveAs, header=None, mode="a") 
+
+def save(saveAs, iteration, gamma, nu, threshold, noise, results, delta):
+    #get values:
+    acc1 = results[0]
+    acc2 = results[1]    
+    accuracy = (results[2][2] + results[3][1]) / (results[2][0] + results[3][0])
+    tp = results[2][2]
+    fp = results[3][2] + results[3][3]
+    fn = results[2][1] + results[2][3]
+    tn = results[3][1]
+    total_in_train = results[2][0]
+    total_not_in_train = results[3][0]
+    fp_3_3 = results[3][3]
+    fn_2_3 = results[2][3]
+                
+    values = [[iteration, gamma, nu, threshold, noise, acc1, acc2, accuracy, tp, fp, fn, tn, total_in_train, total_not_in_train, fp_3_3, fn_2_3, delta]]
+    data = pd.DataFrame(data= values)    
+    data.to_csv(saveAs, header=None, mode="a")  
+
+    
+#CSV file
+saveAs = "./summary_svm_encoder_lstm.csv"
+saveHeader(saveAs)    
+    
+#load the dataset
+pathData = "../../data"
+fileData = "data_cond1_c2"
+
+print("--> loading the datasets")
+dataset = Dataset(pathData, fileData+".csv")
+print("--> datasets ready")
+
+#parameters
+path = "./"
+gammas = np.linspace(0.1, 5, 50)
+nus = np.linspace(0.05, 0.4, 36)
+thresholds = np.linspace(0.0, 0.2, 21) * -1
+iterations=20
+
+
+#Repeat training 
+for i in range(1, iterations+1):
+    print("\n-->Iteration: ", i)
+        
+    #load the embeddings
+    print("loading embeddings")
+    embeddings0 = EmbeddingsDataset(None, dataset)
+    embeddings0.load(path, "iter_" + str(i) + "_embeddings.npy")
+        
+    #load the subsets
+    print ("loading the subsets")
+    dataset.loadSets(path,"iter_" + str(i) + "_dataset.npy")
+                            
+    #Get the random training set
+    randomTrainingSet, randomKnownSet = dataset.selectRandomUnits(dataset.validationSet, 10)
+
+    #Train SVM with different gammas
+    for gamma in gammas:
+        for nu in nus:
+            t0 = time.time()
+            print("--> Train SVM: gamma=", gamma, " nu=", nu)
+            svm = SVM(embeddings0)
+            svm.fit(randomTrainingSet, gamma=gamma, nu = nu)
+                      
+            for tau in thresholds:  
+                #noise 0%
+                svm.embeddings = embeddings0
+                results = svm.accuracy(randomKnownSet, dataset.unseenSet, tau)
+                t1 = time.time()
+                save(saveAs, i, gamma, nu, tau, 0.00, results, t1-t0)
+    
